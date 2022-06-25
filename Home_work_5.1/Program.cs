44f85df07e7563e3c100ef6ee7998ad6da4415ff
Maxim Konev <sayberix.ru@yandex.ru>
2022-06-25 19:49:58 +0700

    Задача 5.5 - выполнена!

--- Home_work_5.1/Program.cs
+++ Home_work_5.1/Program.cs
@@ -4,3 +4,21 @@
 // Блок ввода начального значения
 Console.WriteLine("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
+
+// Выполнение условий
+if (number > 1) // Проверка на валидность начального значения
+{
+    int count = 1;
+    while (count <= number)
+    {
+        if (count % 2 == 0)
+        {
+            Console.Write(count + " ");
+        }
+        count++;
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка! Введеное число должно быть больше 1 !");
+}
