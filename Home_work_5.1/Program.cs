d3340894a9477a1611785b556fbe1147be1ef6bf
Maxim Konev <sayberix.ru@yandex.ru>
2022-06-25 18:35:20 +0700

    Первое условие для задачи

--- Home_work_5.1/Program.cs
+++ Home_work_5.1/Program.cs
@@ -1,7 +1,18 @@
 ﻿// Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-
 Console.WriteLine("Введите первое число:");
 int number_1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if(number_1 <> number_2)
+{
+    if(number_1 > number_2)
+    {
+        Console.WriteLine("Первое введеное число больше! Число: " + number_1 + " больше чем число " + number_2);    
+    }
+}
+else
+{
+    Console.WriteLine("Оба числа одинаковы!");
+}
