108a504ebddba5ace43a650490ad8a5c97dd774b
Maxim Konev <sayberix.ru@yandex.ru>
2022-06-25 18:45:51 +0700

    Блок ввода для задачи 5.3

--- Home_work_5.1/Program.cs
+++ Home_work_5.1/Program.cs
@@ -1,22 +1,8 @@
-﻿// Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 Console.WriteLine("Введите первое число:");
 int number_1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 int number_2 = Convert.ToInt32(Console.ReadLine());
-
-if(number_1 != number_2)
-{
-    if(number_1 > number_2)
-    {
-        Console.WriteLine("Первое введеное число больше! Число " + number_1 + " больше чем число " + number_2);
-    }
-    else
-    {
-        Console.WriteLine("Второе введеное число больше! Число: " + number_2 + " больше чем число " + number_1);
-    }
-}
-else
-{
-    Console.WriteLine("Оба числа одинаковы!");
-}
+Console.WriteLine("Введите третье число:");
+int number_3 = Convert.ToInt32(Console.ReadLine());
