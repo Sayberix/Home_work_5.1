7c7a15a772f853c12fd0930c66f4831c0df3f0fc
Maxim Konev <sayberix.ru@yandex.ru>
2022-06-25 18:39:47 +0700

    Задача выполнена!

--- Home_work_5.1/Program.cs
+++ Home_work_5.1/Program.cs
@@ -5,11 +5,15 @@ int number_1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 int number_2 = Convert.ToInt32(Console.ReadLine());
 
-if(number_1 <> number_2)
+if(number_1 != number_2)
 {
     if(number_1 > number_2)
     {
-        Console.WriteLine("Первое введеное число больше! Число: " + number_1 + " больше чем число " + number_2);    
+        Console.WriteLine("Первое введеное число больше! Число " + number_1 + " больше чем число " + number_2);
+    }
+    else
+    {
+        Console.WriteLine("Второе введеное число больше! Число: " + number_2 + " больше чем число " + number_1);
     }
 }
 else
